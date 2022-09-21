2d17822b807ddf47b3f1ba04e3a1a95c2bd0a658
oshita-n <oshita-n@whispon.com>
2022-09-22 03:03:58 +0900

    ✅ 会社用の文言に変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,26 +15,22 @@ const Home: NextPage = () => {
       <Header />
       <main className={styles.main}>
         <div className={styles.contents}>
-          <h2>大下範晃（NoriakiOshita）</h2>
-          <p>フリーランスのAIエンジニアとして働いています。<br />
-          メタヒューリスティックスを使ったシフト作成AI、図面AI、電力需要予測のデータ分析、<br />
+          <h1>Whispon合同会社</h1>
+          <h2>生活をテクノロジーでもっと楽しくする</h2>
+          <p>メタヒューリスティックスを使ったシフト作成AI、図面AI、電力需要予測のデータ分析、<br />
           自然言語処理を利用したBotAIサービスの構築などの開発に携わってきました。AI開発のほかにもウェブ開発も可能です。</p>
-          <h3>スキル（言語、フレームワークなど）</h3>
-          <p>Python、PHP、JavaScript</p>
-          <p>TensorFlow、PyTorch、Next.jsなど</p>
-          <h3>経歴</h3>
+
+          <h3>経歴 代表社員 大下範晃</h3>
           <ul className={styles.ul}>
             <li>2013年 北海道札幌琴似工業高等学校 電子機械化 卒業</li>
             <li>2018年 北海道情報大学経営情報学部 システム情報学科 卒業</li>
             <li>2018-2019年 AIQ株式会社 機械学習エンジニア</li>
             <li>2019-2020年 株式会社アイデミー データサイエンティスト、コンテンツ編集員</li>
-            <li>2020年- 個人事業主（AI、機械学習、数理最適化、フロントエンドなど）</li>
+            <li>2020年-2022年6月 個人事業主（AI、機械学習、数理最適化、フロントエンドなど）</li>
+            <li>2022年6月- Whispon合同会社 設立</li>
           </ul>
         </div>
-        <div className={styles.contents}>
-          <h2>サービス</h2>
-          <a href="https://otooto.dev">テキスト読み上げサービス「Oto」</a>
-        </div>
+
         <div className={styles.contents}>
           <h2>ブログ</h2>
           <a href="https://qiita.com/NoriakiOshita">Qiitaに書いた記事</a>
