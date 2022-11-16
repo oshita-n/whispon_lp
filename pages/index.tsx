19f385a3d36c03c9a21e64eed2a4413cb9e9cb1f
oshita-n <oshita-n@whispon.com>
2022-11-16 20:31:51 +0900

    ページの更新

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         <div className={styles.contents}>
           <h2>Whispon合同会社<br/>代表社員 大下範晃（NoriakiOshita）</h2>
           <p>
-          メタヒューリスティックスを使ったシフト作成AI、図面AI、電力需要予測のデータ分析、<br />
+          遺伝的アルゴリズムなどのメタヒューリスティックスを使った勤務表作成AI、図面AI、電力需要予測のデータ分析、<br />
           自然言語処理を利用したBotAIサービスの構築などの開発に携わってきました。
           </p>
           <h3>経歴</h3>
@@ -25,6 +25,7 @@ const Home: NextPage = () => {
             <li>2013年 北海道札幌琴似工業高等学校 電子機械化 卒業</li>
             <li>2018年 北海道情報大学経営情報学部 システム情報学科 卒業</li>
             <li>2018-2019年 AIQ株式会社 機械学習エンジニア</li>
+            <li>2019年 株式会社調和技研 機械学習エンジニア</li>
             <li>2019-2020年 株式会社アイデミー データサイエンティスト、コンテンツ編集員</li>
             <li>2020-2022年6月 個人事業主（AI、機械学習、数理最適化、フロントエンドなど）</li>
             <li>2022年6月 Whispon合同会社 代表社員</li>
@@ -37,9 +38,7 @@ const Home: NextPage = () => {
             <li><a href="https://kasupekira.com">技術ブログ</a></li>
             <li><a href="https://qiita.com/NoriakiOshita">Qiita</a></li>
           </ul>
-          <h3>お問い合わせ</h3>
-          <p>AI受託開発のご依頼、AI開発の進め方などに関してのご相談お待ちしております。</p>
-          <p>oshita-n@whispon.com</p>
+          <h3>現在は積極的に仕事の募集はしておりません。</h3>
         </div>
       </main>
 
