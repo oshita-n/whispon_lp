2930e2270f6f1bc61550ce19d6fd1239a12b7cb7
oshita-n <oshita-n@whispon.com>
2023-04-09 18:02:42 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,8 @@ class MyDocument extends Document {
           {/* OGP タグ */}
           <meta property="og:title" content="禅AI" />
           <meta property="og:description" content="禅AIで生きることが楽しくなるプロダクト開発" />
+          <meta property="og:site_name" content="禅AI" />
+          <meta property="og:locale" content="ja_JP" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://whispon.com/" />
           <meta property="og:image" content="https://whispon.com/images/zenAI.png" />
